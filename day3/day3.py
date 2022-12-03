d5370f93df81c3c69ff1a940b87a0068d8dca1c2
Chris Reisor <creisor@gmail.com>
2022-12-03 11:48:25 -0600

    progress on day3

---
+++ day3/day3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+
+class Rucksack(object):
+    def __init__(self, contents: str):
+        self.contents = contents
+
+    def compartment(self, number: int):
+        if number > 2 or number == 0:
+            raise ValueError('rucksacks contain 2 compartments')
+
+        if number == 1:
+            return [self.contents[i] for i in range(0, int(len(self.contents) / 2))]
+        else:
+            print(self.contents.index(self.contents[-1]))
+            return [self.contents[i] for i in range(int(len(self.contents) / 2), self.contents.index(self.contents[-1]))]
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog = 'day3',
+        description = 'Advent of Code 2022: Day N ')
+
+    parser.add_argument('filename')
+    #parser.add_argument('-n', '--number', default=1, type=int, help='some help message about -n')
+    args = parser.parse_args()
+
+    rucksacks = []
+    with open(args.filename) as f:
+        for line in f.readlines():
+            rucksacks.append(line.strip())
+
+    print(rucksacks)
+    r = Rucksack(rucksacks[0])
+    print( r.compartment(1))
+    print( r.compartment(2))
+
+if __name__ == "__main__":
+    main()
