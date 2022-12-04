a128838928b2b16c91edb6b2559a4aec21846792
Chris Reisor <creisor@gmail.com>
2022-12-04 11:47:25 -0600

    adds file reading to the skeleton

--- skel.py
+++ skel.py
@@ -7,9 +7,14 @@ def main():
         prog = 'skeleton',
         description = 'Advent of Code 2022: Day N ')
 
-    parser.add_argument('skeleton')
+    parser.add_argument('filename', description='The inputs file to process')
     parser.add_argument('-n', '--number', default=1, type=int, help='some help message about -n')
     args = parser.parse_args()
+
+    inputs = []
+    with open(args.filename) as f:
+        for line in f.readlines():
+            inputs.append(line.strip())
 
 if __name__ == "__main__":
     main()
