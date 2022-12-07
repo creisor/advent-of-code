612c5509f174205ed2a43096013bce9faeebb36e
Chris Reisor <creisor@gmail.com>
2022-12-06 21:29:11 -0600

    day6 part1 correct answer

--- day6/day6.py
+++ day6/day6.py
@@ -4,11 +4,10 @@ import argparse
 
 def args_and_inputs():
     parser = argparse.ArgumentParser(
-        prog = 'skeleton',
-        description = 'Advent of Code 2022: Day N ')
+        prog = 'day6',
+        description = 'Advent of Code 2022: Day 6')
 
     parser.add_argument('filename', help='The inputs file to process')
-    parser.add_argument('-n', '--number', default=1, type=int, help='some help message about -n')
     args = parser.parse_args()
 
     inputs = []
@@ -18,8 +17,20 @@ def args_and_inputs():
 
     return args, inputs
 
+def get_marker(stream: str):
+    start = 0
+    end = 4
+    while end < len(stream):
+        if len([i for i in stream[start:end] if stream[start:end].count(i) > 1]) == 0:
+            return end
+        start += 1
+        end += 1
+
 def main():
     args, inputs = args_and_inputs()
+
+    for i in inputs:
+        print(get_marker(i))
 
 if __name__ == "__main__":
     main()
