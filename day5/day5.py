f47251abe029cf6b67c17acda5824ac930778aaa
Chris Reisor <creisor@gmail.com>
2022-12-06 19:47:24 -0600

    working day5 part 1

--- day5/day5.py
+++ day5/day5.py
@@ -23,7 +23,7 @@ class Instruction(object):
 class Stack(object):
     def __init__(self, ident: int, items: list):
         self.id = ident
-        self.__items = [re.sub(r'[\[\]]', '', i) for i in items]
+        self.__items = [re.sub(r'[\[\]]', '', i) for i in items if i]
 
     def __str__(self):
         return str(self.id)
@@ -42,7 +42,10 @@ class Stack(object):
 
     @property
     def top(self):
-        return self.items[0]
+        try:
+            return self.items[0]
+        except IndexError:
+            return ''
 
     def remove(self, num):
         items = []
@@ -53,11 +56,9 @@ class Stack(object):
 
     def add(self, items: list):
         crates = [i for i in self.__items if i]
-        print(f'{len(crates)} crates in {self.id} before')
         for i in items:
             crates.insert(0, i)
         self.__items = crates
-        print(f'{len(self.__items)} crates {self.id} after')
 
 def args_and_inputs():
     parser = argparse.ArgumentParser(
@@ -76,70 +77,57 @@ def args_and_inputs():
     return args, inputs
 
 def main():
-    print('move 4 crates from stack 1 to 2')
-    stack1 = Stack(1, ['', '', 'N', 'C', 'B', 'D', 'P'])
-    stack2 = Stack(2, ['T', 'F', 'P', 'L', 'M', 'N', 'W', 'V', 'P', 'H', 'T', 'M'])
+    args, inputs = args_and_inputs()
 
-    print(f'stack1 before: {stack1.items}')
-    print(f'stack2 before: {stack2.items}')
+    # regex for a crate
+    r = re.compile(r'(\[\w\])')
 
-    to_move = stack1.remove(4)
-    stack2.add(to_move)
+    # get the number of colums
+    cols = int(len(re.findall(r'([\w\W])', inputs[0])) / 3)
 
-    print(f'stack1 after: {stack1.items}')
-    print(f'stack2 after: {stack2.items}')
+    rows = []
+    columns = []
+    for line in inputs:
+        if re.match(r'(^ \d)|(^\n)', line):
+            break
+        matches = [i for i in r.finditer(line)]
+        crates = {int(m.span()[0]/4):m.group() for m in matches}
+        print(crates)
 
-    #args, inputs = args_and_inputs()
-    #
-    ## regex for a crate
-    #r = re.compile(r'(\[\w\])')
+        for i in range(0, cols):
+            if crates.get(i):
+                columns.append(crates.get(i))
+            else:
+                columns.append('')
+        rows.append(columns)
+        columns = []
 
-    ## get the number of colums
-    #cols = int(len(re.findall(r'([\w\W])', inputs[0])) / 3)
+    stacks = []
+    for i in range(0, cols):
+        stacks.append(Stack(i+1, [row[i] for row in rows]))
 
-    #rows = []
-    #columns = []
-    #for line in inputs:
-    #    if re.match(r'(^ \d)|(^\n)', line):
-    #        break
-    #    matches = [i for i in r.finditer(line)]
-    #    crates = {int(m.span()[0]/4):m.group() for m in matches}
-    #    print(crates)
+    # regex for an instruction:
+    r = re.compile(r'^move')
+    instructions = []
+    for line in inputs:
+        m = r.search(line)
+        if m:
+            instructions.append(Instruction(line))
 
-    #    for i in range(0, cols):
-    #        if crates.get(i):
-    #            columns.append(crates.get(i))
-    #        else:
-    #            columns.append('')
-    #    rows.append(columns)
-    #    columns = []
+    print([i.items for i in stacks])
+    count = 1
+    for i in instructions:
+        #print([i.items for i in stacks])
+        for stack in stacks:
+            print(f'{stack}: {stack.items}')
+            #stack.print()
+        print()
+        print(f'{count}: move {i.num_crates} crates from stack {i.from_stack} to {i.to_stack}')
+        count += 1
+        to_move = stacks[i.from_stack-1].remove(i.num_crates)
+        stacks[i.to_stack-1].add(to_move)
 
-    #stacks = []
-    #for i in range(0, cols):
-    #    stacks.append(Stack(i+1, [row[i] for row in rows]))
-
-    ## regex for an instruction:
-    #r = re.compile(r'^move')
-    #instructions = []
-    #for line in inputs:
-    #    m = r.search(line)
-    #    if m:
-    #        instructions.append(Instruction(line))
-
-    #print([i.items for i in stacks])
-    #count = 1
-    #for i in instructions:
-    #    #print([i.items for i in stacks])
-    #    for stack in stacks:
-    #        print(f'{stack}: {stack.items}')
-    #        #stack.print()
-    #    print()
-    #    print(f'{count}: move {i.num_crates} crates from stack {i.from_stack} to {i.to_stack}')
-    #    count += 1
-    #    to_move = stacks[i.from_stack-1].remove(i.num_crates)
-    #    stacks[i.to_stack-1].add(to_move)
-
-    #print(f'\n\nanswer: {"".join([s.top for s in stacks])}')
+    print(f'\n\nanswer: {"".join([s.top for s in stacks])}')
 
 if __name__ == "__main__":
     main()
