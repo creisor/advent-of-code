2764f4f7474a1ed90fd5ca0801d4ed50cb264c1f
Chris Reisor <creisor@gmail.com>
2022-12-06 21:35:49 -0600

    day6 part 2 correct answer

--- day6/day6.py
+++ day6/day6.py
@@ -8,6 +8,7 @@ def args_and_inputs():
         description = 'Advent of Code 2022: Day 6')
 
     parser.add_argument('filename', help='The inputs file to process')
+    parser.add_argument('-n', '--number', default=4, type=int, help='number of unique characters which identifies a marker (4 for start-of-packet, 14 for start-of-message)')
     args = parser.parse_args()
 
     inputs = []
@@ -17,9 +18,9 @@ def args_and_inputs():
 
     return args, inputs
 
-def get_marker(stream: str):
+def get_marker(stream: str, distinct_count: int):
     start = 0
-    end = 4
+    end = distinct_count
     while end < len(stream):
         if len([i for i in stream[start:end] if stream[start:end].count(i) > 1]) == 0:
             return end
@@ -30,7 +31,7 @@ def main():
     args, inputs = args_and_inputs()
 
     for i in inputs:
-        print(get_marker(i))
+        print(get_marker(i, args.number))
 
 if __name__ == "__main__":
     main()
