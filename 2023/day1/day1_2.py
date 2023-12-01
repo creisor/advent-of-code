89923ca05dcf1b9b9f729b4fd643d99fe10d521d
creisor <chris.reisor@provi.com>
2023-12-01 14:22:18 -0600

    WIP - first answer right, second wrong

--- 2023/day1/day1_2.py
+++ 2023/day1/day1_2.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+first answer was 55309, which they said was too high...
+"""
+
 import argparse
 import logging
+import re
 
 def args_and_inputs():
     parser = argparse.ArgumentParser(
@@ -11,7 +16,7 @@ def args_and_inputs():
     parser.add_argument('filename', help='The inputs file to process')
     parser.add_argument('-d', '--debug', action=argparse.BooleanOptionalAction, default=False, type=bool, help='enable debug output')
     args = parser.parse_args()
-
+    
     if args.debug:
         logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
     else:
@@ -26,13 +31,31 @@ def args_and_inputs():
 
 def first_last_num(string):
     logging.debug(f'string; {string}')
-    numbers = [str(n) for n in [*range(0,10)]]
+    regex = re.compile(r'(one)|1|(two)|2|(three)|3|(four)|4|(five)|5|(six)|6|(seven)|7|(eight)|8|(nine)|9')
+    num_map = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
 
-    first = [n for n in string if n in numbers][0]
+    matches = regex.finditer(string)
+    matchlist = [m.group() for m in matches]
 
-    rev = [s for s in string]
-    rev.reverse()
-    last = [n for n in str(rev) if n in numbers][0]
+    try:
+        first = num_map[matchlist[0]]
+    except KeyError:
+        first = matchlist[0]
+
+    try:
+        last = num_map[matchlist[-1]]
+    except KeyError:
+        last = matchlist[-1]
 
     firstlast = int(f'{first}{last}')
     logging.debug(f'firstlast: {firstlast}')
--- skel.py
+++ skel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 
 def args_and_inputs():
     parser = argparse.ArgumentParser(
@@ -8,8 +9,14 @@ def args_and_inputs():
         description = 'Advent of Code 2023: Day N ')
 
     parser.add_argument('filename', help='The inputs file to process')
+    parser.add_argument('-d', '--debug', action=argparse.BooleanOptionalAction, default=False, type=bool, help='enable debug output')
     parser.add_argument('-n', '--number', default=1, type=int, help='some help message about -n')
     args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+    else:
+        logging.basicConfig(encoding='utf-8', level=logging.WARNING)
 
     inputs = []
     with open(args.filename) as f:
@@ -20,6 +27,7 @@ def args_and_inputs():
 
 def main():
     args, inputs = args_and_inputs()
+    logging.debug(f'args: {args}\ninputs: {inputs}')
 
 if __name__ == "__main__":
     main()
