0aa00a5f224b2fe69b46f522ca7c28091cb66cb8
Chris Reisor <creisor@gmail.com>
2023-12-01 07:15:34 -0600

    updates year in skeleton

--- skel.py
+++ skel.py
@@ -5,7 +5,7 @@ import argparse
 def args_and_inputs():
     parser = argparse.ArgumentParser(
         prog = 'skeleton',
-        description = 'Advent of Code 2022: Day N ')
+        description = 'Advent of Code 2023: Day N ')
 
     parser.add_argument('filename', help='The inputs file to process')
     parser.add_argument('-n', '--number', default=1, type=int, help='some help message about -n')
